85abc4a371021416896ea9f71029dcc1e28ee13a
saurabhrajguru <srajguru@mobiquityinc.com>
2015-10-06 12:21:12 +0530

    Initial checkin with project and basic sample.

---
+++ ScheduledToastSample/ScheduledToastSample/MainPage.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Data.Xml.Dom;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Notifications;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
+
+namespace ScheduledToastSample
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // TODO: Prepare page for display here.
+
+            // TODO: If your application contains multiple pages, ensure that you are
+            // handling the hardware Back button by registering for the
+            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
+            // If you are using the NavigationHelper provided by some templates,
+            // this event is handled for you.
+        }
+
+        /// <summary>
+        /// Handles the Click event of the Button control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var notifier = ToastNotificationManager.CreateToastNotifier();
+
+            //Removing previous schedules
+            foreach (var scheduledNotification in notifier.GetScheduledToastNotifications())
+                notifier.RemoveFromSchedule(scheduledNotification);
+
+            //Creating xml template for Toast
+            ToastTemplateType toastType = ToastTemplateType.ToastText01;
+            XmlDocument toastXml = ToastNotificationManager.GetTemplateContent(toastType);
+            XmlNodeList toastTextElement = toastXml.GetElementsByTagName("text");
+            toastTextElement[0].AppendChild(toastXml.CreateTextNode("Hi ! This was scheduled"));
+
+            //Creating Scheduled toast and adding it to schedule
+            ScheduledToastNotification scheduleToast = new ScheduledToastNotification(toastXml, DateTime.Today.Add(NotificationTime.Time));
+            notifier.AddToSchedule(scheduleToast);
+
+        }
+    }
+}
